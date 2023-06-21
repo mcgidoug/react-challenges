6d88b06bf6795afa70f6fce5ece72e544aa72e44
mcgidoug <mcgidoug@gmail.com>
2023-06-21 12:08:13 -0500

    updated timer challenge

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -4,15 +4,10 @@ function Timer() {
   const [seconds, setSeconds] = useState(0);
 
   useEffect(() => {
-    // TODO: Implement the timer logic using setInterval
-
-    // Function to increment the seconds state
-    const tick = () => {
-      setSeconds((prevSeconds) => prevSeconds + 1);
-    };
-
     // Start the timer when the component mounts
-    const intervalId = setInterval(tick, 1000);
+    const intervalId = setInterval(() => {
+      setSeconds((prevSeconds) => prevSeconds + 1);
+    }, 1000);
 
     // Clean up the interval when the component unmounts
     return () => {
